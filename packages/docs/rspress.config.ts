8c10f11fd7432711f5de9a877f8515214509f8a6
h7ml <h7ml@qq.com>
2024-04-30 04:04:13 +0800

    ✨ feat: 文档修改

--- packages/docs/rspress.config.ts
+++ packages/docs/rspress.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     light: '/rspress-light-logo.png',
     dark: '/rspress-dark-logo.png',
   },
-  base: '/doc/',
+  base: '/',
   builderConfig: {
     output: {
       // assetPrefix: './amis-adapter/doc/',
